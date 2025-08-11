60e12d66166389d79338d83c78d565e7a6a541df
Shardul Chiplunkar <shardul.chiplunkar@epfl.ch>
2025-08-11 13:29:18 +0200

    Finish SVG rendering (for now)

--- src/main/scala/librrd/LayoutsSVG.scala
+++ src/main/scala/librrd/LayoutsSVG.scala
@@ -22,6 +22,9 @@ object LayoutsSVG extends Layouts[Tag]:
     val bbox = textMetricsElement.getBBox()
     (bbox.width, bbox.height)
 
+  val unitWidth = Layout.unitWidth
+  val radius = 2*unitWidth
+
   override def render(layout: Layout) =
     val inner = layout match
       case rail: Rail => List(line(x1:=0, y1:=0, x2:=rail.width, y2:=0))
@@ -43,21 +46,23 @@ object LayoutsSVG extends Layouts[Tag]:
           g(render(sub), transform:=s"translate($subX,$subY)")
         }
 
-      case ivc @ InlineVerticalConcatenation(sublayouts, marker, tipSpecs, _, extraWidths, _, _) =>
+      case ivc @ InlineVerticalConcatenation(sublayouts, marker, tipSpecs, numRows, extraWidths, _, _) =>
         val (firsts, rests) = sublayouts.splitAt(1)
         val (mids, lasts) = rests.splitAt(rests.length - 1)
         val (first, last) = (firsts(0), lasts(0))
         val offsets = mids.scanLeft(first.height + Layout.rowGap)
           ((offset, sub) => offset + sub.height + Layout.rowGap)
         val padding = InlineVerticalConcatenation.markerPadding
+        val direction = ivc.direction
 
         val inners =
           g(
             render(first),
             text(
               marker,
-              x:=(ivc.direction match { case Direction.LTR => first.width; case Direction.RTL => 0 })
-                + padding, y:=first.tipY(ivc.endSide)),
+              x:=(direction match { case Direction.LTR => first.width; case Direction.RTL => 0 })
+                + padding,
+              y:=first.tipY(ivc.endSide)),
             transform:=s"translate(${extraWidths.left},0)"
           )
           +: mids.zip(offsets).map((mid, offset) =>
@@ -70,24 +75,50 @@ object LayoutsSVG extends Layouts[Tag]:
           :+ g(
             text(
               marker,
-              x:=(ivc.direction match { case Direction.LTR => 0; case Direction.RTL => last.width })
+              x:=(direction match { case Direction.LTR => 0; case Direction.RTL => last.width })
                 + padding,
-              y:=last.tipY(ivc.startSide)
-            ),
+              y:=last.tipY(ivc.startSide)),
             render(last),
             transform:=s"translate(${extraWidths.left},${offsets.last})"
           )
 
-        // TODO: non-default physical tips
-        inners
+        val brackets = List((Side.Left, extraWidths.left, +1),
+                            (Side.Right, extraWidths.left + first.width + ivc.markerWidth, -1))
+          .flatMap((side, x, sign) =>
+            val upwards: Int = -(sign - 1)/2
+            val downwards = 1 - upwards
+            val extraP = SidedProperty.apply.tupled(direction.swap((upwards, downwards)))
+            val tipY = ivc.tipY(side)
+            tipSpecs(side) match
+              case TipSpecification.Physical(p) if p != extraP(side) =>
+                def upPath(subTipY: Double) = path(d:=
+                  s"M ${x - sign*3*unitWidth},$tipY  l ${sign*unitWidth},$tipY "
+                  + s"a $radius,$radius 0 $upwards ${sign*2*unitWidth},${-2*unitWidth} "
+                  + s"L $x,${subTipY + 2*unitWidth} "
+                  + s"a $radius,$radius 0 $downwards ${sign*2*unitWidth},${-2*unitWidth}")
+                def downPath(subTipY: Double) = path(d:=
+                  s"M ${x - sign*3*unitWidth},$tipY  l ${sign*unitWidth},$tipY "
+                  + s"a $radius,$radius 0 $downwards ${sign*2*unitWidth},${2*unitWidth} "
+                  + s"L $x,${subTipY - 2*unitWidth} "
+                  + s"a $radius,$radius 0 $upwards ${sign*2*unitWidth},${2*unitWidth}")
+                val straightPath =
+                  path(d:=s"M ${x - sign*3*unitWidth},$tipY  l ${sign*5*unitWidth},$tipY")
+
+                val (ups, notUps) = (1 to numRows(side))
+                  .map(r => ivc.tipY(side, TipSpecification.Logical(r)))
+                  .partition(_ <= tipY - 4*unitWidth)
+                val (notUpDowns, downs) = notUps.partition(_ <= tipY + 4*unitWidth)
+                val (straights, approxs) = notUpDowns.partition(_ ~= tipY)
+                straights.map(_ => straightPath) ++ ups.map(upPath) ++ downs.map(downPath)
+              case _ => List())
+
+        inners ++ brackets
 
       case bvc @ BlockVerticalConcatenation(
           topSublayout, bottomSublayout, direction, polarity, tipSpecs, numRows, _, _, _) =>
         import TipSpecification.*
         val leftExtraWidth = bvc.extraWidths.left
 
-        val unitWidth = Layout.unitWidth
-        val radius = 2*unitWidth
         val brackets = List((Side.Left, leftExtraWidth, +1),
                             (Side.Right, leftExtraWidth + topSublayout.width, -1))
           .flatMap((side, x, sign) =>
@@ -98,8 +129,6 @@ object LayoutsSVG extends Layouts[Tag]:
               case Polarity.+ => tipSpecs(side) match
                 case Vertical => List()
                 case _ =>
-                  val topmostTipY = bvc.tipY(side, Logical(1))
-                  val bottommostTipY = bvc.tipY(side, Logical(numRows(side)))
                   def upPath(subTipY: Double) = path(d:=
                     s"M ${x - sign*3*unitWidth},$tipY  l ${sign*unitWidth},$tipY "
                     + s"a $radius,$radius 0 $upwards ${sign*2*unitWidth},${-2*unitWidth} "
