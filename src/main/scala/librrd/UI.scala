1c36d8259015094c9cf97c5f4b85f0814108d81d
Shardul Chiplunkar <shardul.chiplunkar@epfl.ch>
2025-08-12 14:50:09 +0200

    Make resizing trigger layout

--- src/main/scala/librrd/UI.scala
+++ src/main/scala/librrd/UI.scala
@@ -26,17 +26,22 @@ object UI:
         "JSON" -> "foo",
         "default" -> "@import url(librrd-default.css);"))
 
+  class Debouncer(private val action: () => Unit, val timeout: Int):
+    private var timer: Option[Int] = None
+    def trigger(): Unit =
+      timer.foreach(dom.window.clearTimeout(_))
+      timer = Some(dom.window.setTimeout(() => action(), timeout))
+
   class InputPresetState(val ip: InputsPresets, val onDone: () => Unit):
-    def presetState(suppressDone: Boolean = false): Unit =
+    private def presetState(suppressDone: Boolean = false): Unit =
       ip.presets.get(ip.preset.value).foreach{ ip.input.value = _ }
       if !suppressDone then onDone()
 
-    var timer: Option[Int] = None
-    def customTypingState(): Unit =
-      timer.foreach(dom.window.clearTimeout(_))
-      timer = Some(dom.window.setTimeout(() => customDoneState(), 500))
+    private val debouncer = Debouncer(() => customDoneState(), 500)
+    private def customTypingState(): Unit =
+      debouncer.trigger()
 
-    def customDoneState(): Unit = onDone()
+    private def customDoneState(): Unit = onDone()
 
     def register(suppressInitialDone: Boolean = false): Unit =
       presetState(suppressInitialDone)
@@ -52,9 +57,28 @@ object UI:
         customTypingState()
       })
 
-  def registerInputsPresets(): Unit =
-    InputPresetState(InputsPresets.Diagram, reLayOut).register(true)
-    InputPresetState(InputsPresets.Layout, reLayOut).register()
+  class ResizeState():
+    var width = 500.0
+    private val debouncer = Debouncer(() => reLayOut(width), 10)
+
+    def register(): Unit =
+      dom.ResizeObserver{ (entries, o) =>
+        entries.foreach{ entry =>
+          if entry.target.id == "output" then
+            val newHeight = entry.borderBoxSize(0).blockSize
+            val newWidth = entry.borderBoxSize(0).inlineSize
+            // default value of preserveAspectRatio takes care of AR
+            outputCanvas.setAttribute("viewBox", s"-10 -10 $newWidth $newHeight")
+            width = newWidth - 20
+            debouncer.trigger()
+        }
+      }.observe(document.getElementById("output"))
+
+  def registerInputs(): Unit =
+    val resize = ResizeState()
+    resize.register()
+    InputPresetState(InputsPresets.Diagram, () => reLayOut(resize.width)).register(true)
+    InputPresetState(InputsPresets.Layout, () => reLayOut(resize.width)).register(true)
     InputPresetState(InputsPresets.Rendering, reRender).register()
 
   given WrappedDiagrams[Tag] = WrappedDiagrams(LayoutsSVG)
@@ -70,34 +94,26 @@ object UI:
         Polarity.+),
       Diagrams.NonterminalToken("diagram")))
 
-  def reLayOut(): Unit =
+  var oldSVG: Option[org.scalajs.dom.Node] = None
+  def reLayOut(width: Double): Unit =
     val myParameterizedDiagram = ParameterizedDiagrams.parameterize(getDiagram, getStylesheet)
     val myDirectedDiagram = DirectedDiagrams.direct(myParameterizedDiagram)
     val myAlignedDiagram = AlignedDiagrams.align(myDirectedDiagram)
     val myWrappedDiagram = summon[WrappedDiagrams[Tag]].wrapLocally(myAlignedDiagram)
-    val myLayout = JustifiedDiagrams.justify(myWrappedDiagram, 500)
+    val myLayout = JustifiedDiagrams.justify(myWrappedDiagram, width)
     val myRendering = summon[WrappedDiagrams[Tag]].backend.render(myLayout)
-    outputCanvas.appendChild(myRendering.render)
-    println("relaidout")
+    val mySVG = myRendering.render
+    if oldSVG.isDefined then
+      outputCanvas.replaceChild(mySVG, oldSVG.get)
+    else
+      outputCanvas.appendChild(mySVG)
+    oldSVG = Some(mySVG)
 
   lazy val customStyleElement = document.getElementById("custom-style")
   def reRender(): Unit =
     customStyleElement.innerHTML = InputsPresets.Rendering.input.value
-    println("rerendered")
-
-  def registerOutputResize(): Unit =
-    dom.ResizeObserver{ (entries, o) =>
-      entries.foreach{ entry =>
-        if entry.target.id == "output" then
-          val height = entry.borderBoxSize(0).blockSize
-          val width = entry.borderBoxSize(0).inlineSize
-          val ypad = 10.0 * height/width
-          entry.target.setAttribute("viewbox", s"-10 -$ypad ${width + 10} ${height + ypad}")
-      }
-    }.observe(document.getElementById("output"))
 
   @main def main(): Unit =
     document.addEventListener("DOMContentLoaded", (event) => {
-      registerInputsPresets()
-      registerOutputResize()
+      registerInputs()
     })
