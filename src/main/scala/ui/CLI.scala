fb9db6d724633315587aaca8ebf1071fa2c39938
Shardul Chiplunkar <shardulc@proton.me>
2025-09-12 00:59:22 +0200

    WIP read Node command-line arguments

--- src/main/scala/ui/CLI.scala
+++ src/main/scala/ui/CLI.scala
@@ -1,7 +1,16 @@
 package ui
 
+import scalajs.js
+import js.annotation.*
+
 object CLI:
+
+  @js.native
+  @JSImport("process", "argv")
+  val argv: js.Array[String] = js.native
+
   @main def main =
+    println(argv.toList.drop(2))
     librrd.LibRRDFile.layOutToSVGFile(
       DiagramParser(GUIPresets.diagramPresets("SQLite table-constraint")).get,
       StylesheetParser(GUIPresets.layoutPresets("SQLite")).get,
