1dbaf39c0df629fbca8eb73ea3c94302ea30d214
Shardul Chiplunkar <shardul.chiplunkar@epfl.ch>
2026-02-05 17:26:44 +0100

    Remove parts not needed for artifact

--- racket/zip.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-build=_zip/
-targetdir=$build/rrd/
-
-rm -fr "$build"
-mkdir -p "$targetdir"
-
-cp README.md librrd.rkt rrd sql-constraint.rrd rrd-gui ../figures/generated/oopsla25.rkt "$targetdir"
-cd $build; zip -r rrd.zip rrd/
